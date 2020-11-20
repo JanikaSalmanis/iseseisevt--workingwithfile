d36cd194a2ce9c11cab6755de639cf65c0e17e4d
Opilane TTHK <opilane@tthk.ee>
2020-11-20 09:37:32 +0200

    first commit

---
+++ Deleting/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Deleting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string roothPath = @"C:\Users\opilane\samples\LOGITpe20-Week12-WorkingWithFiles-Task";
+            DeleteAllfilesinFruit();
+            DeleteAllfilesinVegetables();
+        }
+
+        public static void DeleteAllfilesinFruit()
+        {
+            string roothPath = @"C:\Users\opilane\samples\LOGITpe20-Week12-WorkingWithFiles-Task\fruit";
+            string fileName = "toiletPaper.txt";
+             if (File.Exists(Path.Combine(roothPath, fileName)))
+             {
+                File.Delete(Path.Combine(roothPath, fileName));
+                Console.WriteLine("file deleted.");
+            }else
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+
+        public static void DeleteAllfilesinVegetables()
+        {
+            string roothPath = @"C:\Users\opilane\samples\LOGITpe20-Week12-WorkingWithFiles-Task\vegetables";
+            string fileName = "cheese.txt";
+            if (File.Exists(Path.Combine(roothPath, fileName)))
+            {
+                File.Delete(Path.Combine(roothPath, fileName));
+                Console.WriteLine("file deleted.");
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+    }
+}
---
+++ Iseseisevtöö/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Iseseisevtöö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the fail that you want move to fruit:");
+            string UserInput = Console.ReadLine();
+            MoveToFruit(UserInput);
+            Console.WriteLine("enter the fail that you want move to vegetables:");
+            string UserInput2 = Console.ReadLine();
+            MovetoVegetables(UserInput2);
+        }
+        public static void MoveToFruit(string fileName)
+        {
+            string roothPath = @"C:\Users\opilane\samples\LOGITpe20-Week12-WorkingWithFiles-Task\fruit";
+            string fullFileName = $"{fileName}.txt";
+            string fullFolderPath = Path.Combine(roothPath, fullFileName);
+            File.Create(Path.Combine(roothPath, fullFileName));
+            if (Directory.Exists(fullFolderPath))
+            {
+                Console.WriteLine("The File already exists.");
+            }
+            else
+            {
+                Directory.CreateDirectory(fullFolderPath);
+            }
+
+        }
+
+        public static void MovetoVegetables(string fileName)
+        {
+            string roothPath = @"C:\Users\opilane\samples\LOGITpe20-Week12-WorkingWithFiles-Task\vegetables";
+            string fullFileName = $"{fileName}.txt";
+            string fullFolderPath = Path.Combine(roothPath, fullFileName);
+            File.Create(Path.Combine(roothPath, fullFileName));
+            if (Directory.Exists(fullFolderPath))
+            {
+                Console.WriteLine("The File already exists.");
+            }
+            else
+            {
+                Directory.CreateDirectory(fullFolderPath);
+            }
+        }
+    }
+}
